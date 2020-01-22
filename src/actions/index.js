f6daa7158cc58e7269220fccb8c13d82a699f98e
“Hindreen” <hndfaw@gmail.com>
2020-01-21 20:13:40 -0500

    Revert one commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const setCode = code => ({
   type: 'SET_CODE',
   code
 })
+
+export const setCurrentGuess = guess => ({
+  type: 'SET_CURRENT_GUESS',
+  guess
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { codeReducer } from './codeReducer';
+import { currentGuessReducer } from './currentGuessReducer';
 
 export const rootReducer = combineReducers({
-  codeReducer
+  codeReducer,
+  currentGuessReducer
 })
