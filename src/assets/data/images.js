fdc6a7fc1519c1dbca9d14366a11cd8c823585ea
“Hindreen” <hndfaw@gmail.com>
2020-01-27 08:39:20 -0500

    Refactor instuctions images from hard coded import image to map

---
+++ src/assets/data/images.js
@@ -0,0 +1,49 @@
+import mainImg from "../../assets/images/01-main.jpg";
+import codeImg from "../../assets/images/02-code-.jpg";
+import formImg from "../../assets/images/03-code-.jpg";
+import dontWorryImg from "../../assets/images/04-code-.jpg";
+import formImg1 from "../../assets/images/05-form-.jpg";
+import formImg2 from "../../assets/images/06-form-.jpg";
+import formImg3 from "../../assets/images/07-form-.jpg";
+import formImg4 from "../../assets/images/08-form-.jpg";
+import formImg5 from "../../assets/images/09-form-.jpg";
+import formImg6 from "../../assets/images/10-form-.jpg";
+import formImg7 from "../../assets/images/11-form-.jpg";
+import sideImg from "../../assets/images/12-side-.jpg";
+import roundsImg from "../../assets/images/13-rounds-.jpg";
+import pointsImg1 from "../../assets/images/14-points-.jpg";
+import pointsImg2 from "../../assets/images/15-points-.jpg";
+import pointsImg3 from "../../assets/images/16-points-.jpg";
+import succRoundsImg from "../../assets/images/17-succ-rounds-.jpg";
+import diffLevelImg1 from "../../assets/images/18-diff-level-.jpg";
+import diffLevelImg2 from "../../assets/images/19-diff-level-.jpg";
+import hintsImg1 from "../../assets/images/20-hints-.jpg";
+import hintsImg2 from "../../assets/images/21-hints-.jpg";
+import instImg from "../../assets/images/22-instruction-.jpg";
+
+const data = [
+    mainImg,
+    codeImg,
+    formImg,
+    dontWorryImg,
+    formImg1,
+    formImg2,
+    formImg3,
+    formImg4,
+    formImg5,
+    formImg6,
+    formImg7,
+    sideImg,
+    roundsImg,
+    pointsImg1,
+    pointsImg2,
+    pointsImg3,
+    succRoundsImg,
+    diffLevelImg1,
+    diffLevelImg2,
+    hintsImg1,
+    hintsImg2,
+    instImg
+]
+
+export default data;
